aa156aaa64538b070221b3a05d5944d173ebd01f
Edgar Villegas <edgar.villegas@lalagunatek.com>
2026-09-23 00:36:01 -0500

    refactor: file and code for dune rock world controller

--- horizon-worlds/dunerock/scripts/122339131952000587/scripts/DuneRockWorldController.ts
+++ horizon-worlds/dunerock/scripts/122339131952000587/scripts/DuneRockWorldController.ts
@@ -1,12 +1,26 @@
 import * as hz from "horizon/core";
 
+enum WorldLifecycleState {
+  Initializing = "INITIALIZING",
+  GeneratingFloor = "GENERATING_FLOOR",
+  FloorReady = "FLOOR_READY",
+  GeneratingRoads = "GENERATING_ROADS",
+  RoadsReady = "ROADS_READY",
+  WorldReady = "WORLD_READY",
+}
+
 export class DuneRockWorldController extends hz.Component<
   typeof DuneRockWorldController
 > {
   static propsDefinition = {};
 
+  private currentState: WorldLifecycleState =
+    WorldLifecycleState.Initializing;
+
   start() {
-    console.log("[DuneRock] World controller started.");
+    console.log(
+      `[DuneRock] World controller started. State: ${this.currentState}`
+    );
   }
 }
 
