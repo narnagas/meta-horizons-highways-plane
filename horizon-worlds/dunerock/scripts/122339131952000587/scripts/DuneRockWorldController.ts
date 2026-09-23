cd090c92b0e3dc15dcc34edc2355fa72429cb90e
Edgar Villegas <edgar.villegas@lalagunatek.com>
2026-09-23 02:06:04 -0500

    feat: update connect world controller to floor generator

--- horizon-worlds/dunerock/scripts/122339131952000587/scripts/DuneRockWorldController.ts
+++ horizon-worlds/dunerock/scripts/122339131952000587/scripts/DuneRockWorldController.ts
@@ -1,4 +1,4 @@
-import { FloorGenerator } from "floorGenerator";
+import { FloorGenerator } from "./floorGenerator";
 import * as hz from "horizon/core";
 
 enum WorldLifecycleState {
@@ -22,23 +22,27 @@ export class DuneRockWorldController extends hz.Component<
   private currentState: WorldLifecycleState =
     WorldLifecycleState.Initializing;
 
-  start() {
-    console.log(
-      `[DuneRock] World controller started. State: ${this.currentState}`
+start() {
+  console.log(
+    `[DuneRock] World controller started. State: ${this.currentState}`
+  );
+
+  this.transitionTo(WorldLifecycleState.GeneratingFloor);
+
+  const floorGenerator =
+    this.props.floorGenerator
+      ?.getComponents(FloorGenerator)[0];
+
+  if (!floorGenerator) {
+    console.error(
+      "[DuneRock] FloorGenerator component was not found."
     );
-    
-    this.transitionTo(WorldLifecycleState.GeneratingFloor);
 
-    const floorGenerator = this.props.floorGenerator?.getComponents(FloorGenerator)[0];
-
-    if(!floorGenerator){
-      console.error("[DuneRock] FloorGenerator component was not found.");
-
-      return;
-    }
-
-    floorGenerator.generate();
+    return;
   }
+
+  floorGenerator.generate();
+}
 
   private transitionTo(nextState: WorldLifecycleState): void {
     const previousState = this.currentState;
